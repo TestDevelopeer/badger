c3786033b601b641c3c85f236609037337abea51
yessir <tdevelopeer@gmail.com>
2025-07-09 15:11:45 +0300

    API Routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        // Get Articles either by ID or Slug
+        Route::bind('articles', function ($value) {
+            return \App\Models\Article::where('id', $value)
+                ->orWhere('slug', $value)
+                ->firstOrFail();
         });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\V1\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function () {
+    // Articles
+    Route::apiResource('/articles', ArticleController::class);
+
+    // Author
+    Route::get('/authors/{user}', [ArticleController::class, 'show'])->name('authors');
 });
