6e1f5978102af703a882f65e6f2bc4a01a01f406
yessir <tdevelopeer@gmail.com>
2025-07-11 10:40:30 +0300

    Postman - "CSRF Token"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
-            '/login',
+            'api/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
